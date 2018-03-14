62f9023b8ca3230969beeebdd4c6ee55b0b5b7b6
herediavaleria <36711930+herediavaleria@users.noreply.github.com>
2018-03-14 17:30:51 -0300

    NO SE Q HICE ACA JAJAJ

--- Administracion/FORMULARIOS/FRM_FORMAS_DE_PAGO.vb
+++ Administracion/FORMULARIOS/FRM_FORMAS_DE_PAGO.vb
@@ -1,32 +1,71 @@
 ﻿Public Class FRM_FORMAS_DE_PAGO
     Dim datacontext As New DC_AdminDataContext
+
+    'GUARDAR
     Private Sub BTN_FDP_GUARDAR_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BTN_FDP_GUARDAR.Click
-        Dim BUSCARFDP = (From FDP In datacontext.FORMAS_DE_PAGO Select FDP.FDP_DESCRIPCION Where FDP_DESCRIPCION = TB_FDP_DESCRIPCION.Text).Any
-        If BUSCARFDP = True Then
-            MsgBox("La forma de pago ingresada ya existe")
+        Dim BUSCARFORMAPAGO = (From FDP In datacontext.FORMAS_DE_PAGO Select FDP.FDP_DESCRIPCION Where FDP_DESCRIPCION = TB_FDP_DESCRIPCION.Text).Any
+        If BUSCARFORMAPAGO = True Then
+            MsgBox("El DNI ingresado ya pertence a un cliente")
+            Exit Sub
+        End If
+
+        If TB_FDP_DESCRIPCION.Text.Length = 0 Then
+            MsgBox("Debe completar todos los campos requeridos")
             Exit Sub
         End If
         Try
-            If TB_FDP_DESCRIPCION.Text.Length = 0 Then
-                MsgBox("Debe completar todos los campos requeridos")
-                Exit Sub
-            Else
-                Dim I As Integer
-                Dim FDEPAGO = New FORMAS_DE_PAGO
+            Dim i As Integer
+            Dim FORMA_PAGO = New FORMAS_DE_PAGO
 
-                FDEPAGO.FDP_DESCRIPCION = TB_FDP_DESCRIPCION.Text
+            '  FORMA_PAGO.ID_FORM_DE_PAGO = TB_FDP_ID.Text
+            FORMA_PAGO.FDP_DESCRIPCION = TB_FDP_DESCRIPCION.Text
 
-                I = FDEPAGO.ID_FORM_DE_PAGO
+            i = FORMA_PAGO.ID_FORM_DE_PAGO
 
-                datacontext.FORMAS_DE_PAGO.InsertOnSubmit(FDEPAGO)
-                datacontext.SubmitChanges()
-                MsgBox("La forma de pago se ha creado correctamente", vbInformation)
+            datacontext.FORMAS_DE_PAGO.InsertOnSubmit(FORMA_PAGO)
+            datacontext.SubmitChanges()
 
-            End If
+               Select MsgBox("El Cliente fue creado, cargar otro?", MsgBoxStyle.Information + MsgBoxStyle.YesNo, "Nuevo cliente")
+                Case MsgBoxResult.No
+                    Me.Close()
+                Case MsgBoxResult.Yes
+                    TB_FDP_DESCRIPCION.Clear()
+            End Select
+
         Catch ex As Exception
-            MsgBox("Debe completar todos los campos requeridos")
+            MsgBox("La forma de pago no fue cargada, pongase en contacto con el administrador")
             Exit Sub
         End Try
+
     End Sub
 
+    Private Sub FRM_FORMAS_DE_PAGO_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        TB_FDP_DESCRIPCION.Focus()
+    End Sub
+
+    Private Sub BTN_FDP_SALIR_Click(sender As System.Object, e As System.EventArgs) Handles BTN_FDP_SALIR.Click
+        Me.Close()
+    End Sub
+
+    Private Sub BTN_FDP_ACTUALIZAR_Click(sender As System.Object, e As System.EventArgs) Handles BTN_FDP_ACTUALIZAR.Click
+        If TB_FDP_DESCRIPCION.Text.Length = 0 Then
+            MsgBox("Debe completar todos los campos requeridos")
+            Exit Sub
+        End If
+        Try
+            Dim ActualizarFormaPago = (From P In datacontext.FORMAS_DE_PAGO Where P.ID_FORM_DE_PAGO = (TB_FDP_ID.Text)).ToList()(0)
+
+            ActualizarFormaPago.ID_FORM_DE_PAGO = TB_FDP_ID.Text
+            ActualizarFormaPago.FDP_DESCRIPCION = TB_FDP_DESCRIPCION.Text
+           
+            datacontext.SubmitChanges()
+            MsgBox("Los datos se han modificado correctamente")
+
+            FRM_CLIENTES_BUSCAR_B_M.cargargrilla()
+            Me.Close()
+
+        Catch ex As Exception
+            MsgBox("Los datos no se han modificado! intente nuevamente", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "Modificar alumno")
+        End Try
+    End Sub
 End Class
